5131c9ec233fd2873ca12f9d0695b5a45f7979d6
Timur Yurov <litrik05@gmail.com>
2025-06-15 09:06:28 +0300

    Create Palladin and Warrior

--- classes.py
+++ classes.py
@@ -7,3 +7,16 @@ class Thing:
         self.attack = attack
         self.hp = hp
 
+class Person:
+    pass
+
+
+class Palladin(Person):
+    def __init__(self, name,  hp, base_attack, base_protect):
+        super().__init__(name, hp * 2, base_attack , base_protect * 2)
+
+
+
+class Warrior(Person)
+    def __init__(self, name, hp, base_attack, base_protect)
+        super.__init__(name, hp, base_attack * 2, base_protect)
