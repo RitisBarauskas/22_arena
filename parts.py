3a4b29f7b34060bccabad544d9a7c8b253867f22
Ekaterina Voronova <ekaterinaponurova@ya.ru>
2025-06-15 09:57:24 +0300

    add set things

--- parts.py
+++ parts.py
@@ -1,5 +1,20 @@
-class Person:
+THINGS = [
+"Меч 'Секретный ингредиент – боль'",
+"Щит для прятания от мамы",
+"Меч 'Ой, кажется, я промахнулся'",
+"Щит 'Защита от дурацких решений'",
+]
 
+
+class Thing:
+    def __init__(self, name, protection, attack, hp):
+        self.name = name
+        self.protection = protection
+        self.attack = attack
+        self.hp = hp
+
+
+class Person:
     def __init__(self, name, hit_points, base_attack, protection_percentage):
         self.name = name
         self.hit_points = hit_points
@@ -8,25 +23,30 @@ class Person:
         self.things = []
 
 
-    def set_things(self, things):
-        """
-        Устанавливает список предметов, которые носит персонаж.
-        """
-        pass
+    def set_things(self, things: Thing):
+        self.things = things.copy()
+        self.current_hit_points = self.hit_points + sum(thing.hp for thing in self.things)
+
 
     def get_final_protection(self):
         """
         Вычисляет общий процент защиты.
         """
-        pass
-
-    def calculate_damage(self):
-      """Вычитания урона"""
-       pass
-
+        return self.base_protection + sum(t.protection for t in self.things)
 
     def subtract_life(self, damage):
         """
         Вычитает урон из текущего количества жизней.
         """
-        pass
+        self.hit_points -= (damage - damage * self.get_final_protection())
+        return self.hit_points
+
+
+class Paladin(Person):
+    def __init__(self, name,  hp, base_attack, base_protect):
+        super().__init__(name, hp * 2, base_attack, base_protect * 2)
+
+
+class Warrior(Person):
+    def __init__(self, name, hp, base_attack, base_protect):
+        super().__init__(name, hp, base_attack * 2, base_protect)
