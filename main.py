3da1a8e6369a2a7d023155a2d5707c75e2414f95
Gurin Valeriy <gurin.valerii@gmail.com>
2025-06-15 14:32:51 +0600

    main 06 commit

--- main.py
+++ main.py
@@ -28,4 +28,3 @@ if __name__ == '__main__':
             print('Битва окончена, победитель определён!')
             break
     print(f'Новый чемпион - {arena.players[0].name} !')
-    time.sleep(5)
