053b82856a39cf61a73848c84bea08037d1c980b
Ekaterina Voronova <ekaterinaponurova@ya.ru>
2025-06-15 10:18:28 +0300

    add final_attack and is_alive

--- parts.py
+++ parts.py
@@ -5,7 +5,6 @@ THINGS = [
 "Щит 'Защита от дурацких решений'",
 ]
 
-
 class Thing:
     def __init__(self, name, protection, attack, hp):
         self.name = name
@@ -17,22 +16,28 @@ class Thing:
 class Person:
     def __init__(self, name, hit_points, base_attack, protection_percentage):
         self.name = name
-        self.hit_points = hit_points
+        self.current_hit_points = self.hit_points
         self.attack_damage = base_attack
         self.base_protection = protection_percentage
         self.things = []
 
-
-    def set_things(self, things: Thing):
+    def set_things(self, things):
+        """
+        Устанавливает список предметов, которые носит персонаж.
+        """
+        if len(things) > 4:
+            things = things[:4]
         self.things = things.copy()
-        self.current_hit_points = self.hit_points + sum(thing.hp for thing in self.things)
-
 
     def get_final_protection(self):
         """
         Вычисляет общий процент защиты.
         """
         return self.base_protection + sum(t.protection for t in self.things)
+
+    def final_attack(self) -> int:
+        """Общая атака с учетом вещей"""
+        return self.base_attack + sum(thing.attack for thing in self.things)
 
     def subtract_life(self, damage):
         """
@@ -41,6 +46,9 @@ class Person:
         self.hit_points -= (damage - damage * self.get_final_protection())
         return self.hit_points
 
+    def is_alive(self) -> bool:
+        """Проверка, жив ли персонаж"""
+        return self.current_hit_points > 0
 
 class Paladin(Person):
     def __init__(self, name,  hp, base_attack, base_protect):
