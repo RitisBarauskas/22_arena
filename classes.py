4268d71205a79d335ccc22db8fcf77a918f73a28
Timur Yurov <litrik05@gmail.com>
2025-06-15 08:43:50 +0300

    Create Thing class

---
+++ classes.py
@@ -0,0 +1,9 @@
+
+
+class Thing:
+    def __init__(self, name, protection, attack, hp):
+        self.name = name
+        self.protection = protection
+        self.attack = attack
+        self.hp = hp
+
